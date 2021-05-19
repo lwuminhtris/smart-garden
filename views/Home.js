7324c2fca19033226db6e9e253366eb9ed670b64
Minh Tri <minhtri.luu10@gmail.com>
2021-05-20 00:31:12 +0700

    home first design implemented

--- views/Home.js
+++ views/Home.js
@@ -13,7 +13,125 @@ import { createStackNavigator } from '@react-navigation/stack'
 export default function Home({ navigation }) {
   return (
     <SafeAreaView style = {styles.container}>
+      <View
+        style = {{
+          width: '90%',
+        }}
+      >
+        <Text
+          style = {{
+            fontSize: 26,
+            fontWeight: 'bold',
+          }}
+        >
+          Chào Minh Trí
+        </Text>
+      </View>
 
+      
+      <View
+        style = {styles.standardView}        
+      > 
+        <TouchableOpacity
+          style = {{
+              width: '35%',
+              height: '100%',
+              backgroundColor: 'white',
+              elevation: 10,
+              borderRadius: 10,
+          }}
+        >
+          <Text>
+            Độ ẩm
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style = {{
+              width: '60%',
+              height: '100%',
+              backgroundColor: 'white',
+              elevation: 10,
+              borderRadius: 10,
+          }}
+        >
+          <Text>
+            Nhiệt độ
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View
+        style = {styles.standardView}
+      >
+        <TouchableOpacity
+          style = {{
+              width: '100%',
+              height: '100%',
+              backgroundColor: 'white',
+              elevation: 10,
+              borderRadius: 10,
+          }}
+        >
+          <Text>
+            Mức nước
+          </Text>
+        </TouchableOpacity>        
+      </View>
+
+      <View
+        style = {styles.standardView}
+      >
+        <TouchableOpacity
+          style = {{
+              width: '100%',
+              height: '100%',
+              backgroundColor: 'white',
+              elevation: 10,
+              borderRadius: 10,
+          }}
+        >
+          <Text>
+            Cường độ sáng
+          </Text>
+        </TouchableOpacity>        
+      </View>
+
+      <View
+        style = {styles.standardView}
+      >
+        <TouchableOpacity
+          style = {{
+              width: '100%',
+              height: '100%',
+              backgroundColor: 'white',
+              elevation: 10,
+              borderRadius: 10,
+          }}
+        >
+          <Text>
+            Thống kê chi tiết
+          </Text>
+        </TouchableOpacity>        
+      </View>
+
+      <View
+        style = {styles.standardView}
+      >
+        <TouchableOpacity
+          style = {{
+              width: '100%',
+              height: '100%',
+              backgroundColor: 'white',
+              elevation: 10,
+              borderRadius: 10,
+          }}
+        >
+          <Text>
+            Điều khiển thiết bị
+          </Text>
+        </TouchableOpacity>        
+      </View>
     </SafeAreaView>
   );
 }
@@ -21,13 +139,16 @@ export default function Home({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
     flexWrap: 'nowrap',
-    // backgroundColor:  '#0EAD69',
+    backgroundColor: '#F5FDFB',
   },
   standardView : {
-
+    width: '90%',
+    height: 110,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
   }
 });
